4f49596a840b4ba8e321602174e06fa63cae42ad
RanulRathnayake <rathnayakerg.20@uom.lk>
2024-07-13 01:46:09 +0530

    Post, Put, Delete Request Apis

--- src/main/java/com/ranul/RestAPI/testing/controller/CloudVendorAPIService.java
+++ src/main/java/com/ranul/RestAPI/testing/controller/CloudVendorAPIService.java
@@ -2,22 +2,39 @@ package com.ranul.RestAPI.testing.controller;
 
 
 import com.ranul.RestAPI.testing.model.CloudVendor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/CloudVendor")
 public class CloudVendorAPIService {
 
+    CloudVendor cloudVendor;
+
     @GetMapping("{vendorID}")
     public CloudVendor getCloudVendorDetails(String vendorID)
     {
-        return new CloudVendor(
-                "C1",
-                "vendor01",
-                "Address01",
-                "xxxxxx"
-        );
+        return cloudVendor;
+                //new CloudVendor("C1","vendor01","Address01","xxxxxx");
+    }
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        this.cloudVendor=cloudVendor;
+        return "Cloud Vendor Created Successfully";
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        this.cloudVendor=cloudVendor;
+        return "Cloud Vendor Updated Successfully";
+    }
+
+    @DeleteMapping("{vendorID}")
+    public String deleteCloudVendorDetails(String vendorID)
+    {
+        this.cloudVendor=null;
+        return "Cloud Vendor Deleted Successfully";
     }
 }
